6aa21f0629a51f13ea39314cfe86fc6e30249d6a
Aldayr <angelaldayr@gmail.com>
2019-09-28 08:59:01 -0500

    Practica login

--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.page.scss'],
 })
 export class RegistrarPage implements OnInit {
+  registrarForm: FormGroup;
+  submitted = false;
+  
+  constructor(private formBuilder: FormBuilder) { 
 
-  constructor() { }
+    this.registrarForm=this.formBuilder.group({
+      nombre: ['',Validators.required],
+      correo: ['',[Validators.required,Validators.email]],
+      contrasena: ['',[Validators.required,Validators.minLength(6)]],
+      confirmacionContrasena: ['',Validators.required]
+  })
+}
 
   ngOnInit() {
+/*
+  },{
+      validator: this.MustMatch('password','confirmarPassword')
+      
+    });*/
   }
 
+  get f() {return this.registrarForm.controls;}
+  registrar(){
+    console.log(this.registrarForm.valid);
+  }
+  /*
+  registrar(form){
+    let inputs = form.form.value;
+    if(inputs.password != inputs.confirmarPassword){
+      console.log('el password y la confirmacion no son iguales');
+    }
+  }
+
+  MustMatch(controlName: string, matchingControlName: string){
+    return true;
+  }
+*/
 }
